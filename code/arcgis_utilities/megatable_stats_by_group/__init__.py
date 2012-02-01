6f9f52c32389fc850ec20e3948b1922d79f69f91
Nick Santos <nickrsan@users.noreply.github.com>
2012-01-31 17:26:41 -0800

    Fixes to the stats tool

--- code/arcgis_utilities/megatable_stats_by_group/__init__.py
+++ code/arcgis_utilities/megatable_stats_by_group/__init__.py
@@ -24,20 +24,22 @@ except:
 	print "Your Python installation must have a version of scipy installed"
 	raise
 
-class table:
+class cl_table:
 	def __init__(self):
 		self.name = None
 		self.table = None
 		self.groups = []
 
-class group:
+class cl_group:
 	def __init__(self):
 		self.name = None
 		self.columns = []
 		self.col_parts = []
 		self.data = [] # doesn't actually need to be associated with the columns here
 		self.col_name_base = None # assigned by the script - references the appropriate column in the data file after it's created - true col names include _mean or _std at the end
-		
+		self.mean_col = None
+		self.std_col = None
+		self.n_col = None
 		
 input_tables = arcpy.GetParameter(0)
 input_groups = arcpy.GetParameter(1)
@@ -57,7 +59,8 @@ def input_tables_to_list(inputs):
 	new_list = []
 	
 	while cur_row  < num_rows:
-		new_list.append(inputs.getRow(cur_row))
+		new_list.append(str(inputs.getRow(cur_row)[1:-1])) # we slice from 1:-1 because it puts '' around the name! Very confusing to debug...
+		cur_row += 1
 	
 	return new_list
 
@@ -71,7 +74,7 @@ def arc_groups_to_lists(arc_value_table):
 	cur_row = 0
 	
 	while cur_row  < num_groups:
-		temp_row = arc_value_table.getRow(cur_row)
+		temp_row = arc_value_table.getRow(cur_row)[1:-1]
 		groups.append(temp_row.split(','))
 		cur_row += 1
 		
@@ -80,12 +83,12 @@ def arc_groups_to_lists(arc_value_table):
 def make_groups(tables,groups):
 	all_tables = []
 	for table in tables:
-		l_table = table()
+		l_table = cl_table()
 		l_table.table = table
 		
 		ids = 1
 		for group in groups:
-			l_group = group()
+			l_group = cl_group()
 			l_group.col_parts = group # assign the input parts here
 			l_group.name = ids
 			ids += 1
@@ -97,19 +100,23 @@ def make_groups(tables,groups):
 
 def assign_columns_to_groups(tables):
 	for table in tables: # for every table
-		table_fields = arcpy.ListFields(table.table) # get the columns for the table
+		table_fields = arcpy.ListFields(str(table.table)) # get the columns for the table
 		for field in table_fields: # for every field
 			for group in table.groups: # look in the column parts for the group
 				for item in group.col_parts:
-					if field.find(item) > -1: # if that item is present in the field name
-						group.columns.append(field) # then add that field to the group's info
-						
+					if field.name.find(item) > -1: # if that item is present in the field name
+						group.columns.append(field.name) # then add that field to the group's info
+				
 def load_and_process_data(tables):						
 	for table in tables:
 		for group in table.groups: # add the columns
 			group.col_name_base = "group_%s_%s" % (group.name,group.col_parts[0])
-			arcpy.AddField_management(table.table,"%s_mean" % group.col_name_base,"DOUBLE")
-			arcpy.AddField_management(table.table,"%s_std" % group.col_name_base,"DOUBLE")			
+			group.mean_col = "%s_mean" % group.col_name_base
+			group.std_col = "%s_std" % group.col_name_base
+			group.n_col = "%s_n" % group.col_name_base
+			arcpy.AddField_management(table.table,group.mean_col,"DOUBLE")
+			arcpy.AddField_management(table.table,group.std_col,"DOUBLE")
+			arcpy.AddField_management(table.table,group.n_col,"INTEGER")						
 			
 			l_curs = arcpy.UpdateCursor(table.table)
 			for row in l_curs:
@@ -119,18 +126,24 @@ def load_and_process_data(tables):
 				group.data.append(t_row_data)
 				
 				t_sci_array = scipy.array(t_row_data)
-				row.setValue("%s_mean" % group.col_name_base,t_sci_array.mean)
-				row.setValue("%s_std" % group.col_name_base,t_sci_array.std)
-			
-				row.updateRow()
+				#arcpy.AddMessage("Mean: [%s] , Std: [%s] " % (t_sci_array.mean(),t_sci_array.std()))
+				row.setValue(group.mean_col,t_sci_array.mean())
+				row.setValue(group.std_col,t_sci_array.std())
+				row.setValue(group.n_col,t_sci_array.size)
+				
+				l_curs.updateRow(row)
+				
 			del row
 			del l_curs
 
-
+arcpy.AddMessage("Converting inputs")
 input_tables = input_tables_to_list(input_tables) # do some data conversion
 input_groups = arc_groups_to_lists(input_groups) # do some data conversion
+arcpy.AddMessage("Setting up data")
 all_tables = make_groups(input_tables,input_groups) # make the group objects for the tables and columns
+arcpy.AddMessage("Assigning Groups")
 assign_columns_to_groups(all_tables) # load the columns from each table and assign columns to groups based upon names
+arcpy.AddMessage("Loading and processing data")
 load_and_process_data(all_tables)
 
 arcpy.AddMessage("Complete - the columns were added to the existing tables")
