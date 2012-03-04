49563926ceae02175bd3ed505a6e73f32fad1b97
Nick Santos <nickrsan@users.noreply.github.com>
2012-03-03 17:36:47 -0800

    New common code modules and changed transform.py to use the math module's min_max

---
+++ code/common/math.py
@@ -0,0 +1,44 @@
+'''
+Created on Mar 3, 2012
+
+@author: Nick
+'''
+
+class min_max():
+	def __init__(self):
+		self.min = 0
+		self.max = 0
+		self.n = 0
+
+	def track(self,item):
+		
+		item = self.mm_coerce(item)
+
+		if item < self.min:
+			self.min = item
+		elif item > self.max:
+			self.max = item
+			
+		self.n += 1
+	
+	def report(self):
+		print "Min: %s, Max: %s, n: %s" % (self.min,self.max,self.n)
+	
+	def stretch(self,s_value,low = 0,high = 1):
+		s_value = self.mm_coerce(s_value)
+			
+		new_val = (( (s_value - self.min)/(self.max - self.min)) * (high-low) + low) # conduct adjustment
+		return new_val
+	
+	def mm_coerce(self,item):
+		
+		if type(item) != "int" and type(item) != "float":
+			try:
+				item = int(item) # coerce it into into
+			except:
+				try:
+					item = float(item)
+				except:
+					raise TypeError("Couldn't coerce into int or float - we're not going to order a string for you!")
+		
+		return item
--- code/transform_ascii_lidar_1.0/transform.py
+++ code/transform_ascii_lidar_1.0/transform.py
@@ -2,6 +2,8 @@ import os
 import sys
 import re
 
+from code_library.common.math import min_max 
+mm = min_max()
 
 try:
 	wdir = os.getcwd()
@@ -27,33 +29,39 @@ except:
 	log("Couldn't get contents of input directory")
 
 for cur_file in all_files:
-        try:
-        	log("Processing file %s" % cur_file)
-                data = open(os.path.join(input_dir,cur_file),'r')
-        	output = open(os.path.join(output_dir,cur_file),'w')
-        except:
-                log("Couldn't load file - skipping")
-                continue
+	try:
+		log("Processing file %s" % cur_file)
+		data = open(os.path.join(input_dir,cur_file),'r')
+		output = open(os.path.join(output_dir,cur_file),'w')
+	except:
+		log("Couldn't load file - skipping")
+		continue
 
-        try:
-        	output.write("x y z i r g b\n")
-                for line in data:
-                        match = re.search('^(\d*)\s*$',line)
-                        if match is not None and match.group(0) is not None:
-                                continue # we don't care about this line
-                        else:
-                                output.write("%s" % line)
-        except:
-                log("Couldn't read or write data - skipping file")
-                continue
+	try:
+		output.write("x y z i r g b\n")
+		for line in data:
+			match = re.search('^(\d*)\s*$',line)
+			if match is not None and match.group(0) is not None:
+				continue # we don't care about this line
+			else:
+			 	newmatch = re.search('^\d+\s+\d+\s+\d+\s+(\d+\s+)') # find the fourth group of numbers
+			 	if newmatch is not None and newmatch.group(0) is not None:
+			 		mm.track(newmatch.group(0))
+				else:
+					log("Problem matching line for intensity minmax")
+				output.write("%s" % line)
+	except:
+		log("Couldn't read or write data - skipping file")
+		continue
 
-        try:
-                log("File complete - located at %s\n" % os.path.join(output_dir,cur_file))        
-                output.close()
-                data.close()
-        except:
-                log("Couldn't close files - it's probably ok though - they'll get loaded again for the next file")
-                continue
+	try:
+		log("File complete - located at %s\n" % os.path.join(output_dir,cur_file))	
+		output.close()
+		data.close()
+		mm.report()
+	except:
+		log("Couldn't close files - it's probably ok though - they'll get loaded again for the next file")
+		continue
 
 log_file.close()
 
