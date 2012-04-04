1586c62a3575796b4ceb1cd0272b6118c6de1a29
Nick Santos <nickrsan@users.noreply.github.com>
2012-04-04 16:53:21 -0700

    Additions from the last commit - adding in the new files related to the path name changes

---
+++ releases/cws_toolbox/cws_tbx_1.2/cws_toolbox/transform_lidar/report_stats.py
@@ -0,0 +1,28 @@
+import sys
+import arcpy
+
+from cws_toolbox.transform_lidar.cwslidar import *
+
+class func_wrapper:
+
+	def __init__(self):
+		pass
+
+	def processor(self,data,mm,output = None): # mm is the minmax object
+		for line in data:
+			try:
+				newmatch = re.search('^\d+\.?\d*\s+\d+\.?\d*\s+\d+\.?\d*\s+(-?\d+)\s+',line) # find the fourth group of numbers and include the sign
+				if newmatch is not None and newmatch.group(1) is not None:
+					mm.track(newmatch.group(1))
+				else:
+					log("line dropped - Problem matching line for intensity minmax - %s" % line)
+			except:
+				log("Problem reading and scaling intensity")
+		
+		log(mm.report(return_text = True))
+
+setup()
+reporter = func_wrapper()
+process_data(reporter)
+
+shutdown()
