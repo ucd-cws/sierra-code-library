0940d242ad10f8e3bffdd9b80040acadc07dff41
Nick Santos <nickrsan@users.noreply.github.com>
2012-10-04 15:14:22 -0700

    Adding the start of the distutils script, and a few other fixes

---
+++ code/cws_toolbox/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="CWS Toolbox",
+	version="1.3",
+	description="The UCD CWS Code Library and ArcGIS toolbox",
+	packages=['code_library','cws_toolbox'],
+	author="Nick Santos, with contributions from other staff",
+	author_email="nrsantos@ucdavis.edu",)
