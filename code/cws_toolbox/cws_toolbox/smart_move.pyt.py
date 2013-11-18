650f48425d68e21bcc0080544af0aad8b5160470
Nick Santos <nickrsan@users.noreply.github.com>
2013-11-18 06:56:56 -0800

    Restructure of the new toolboxes and setup for a 10.0 compatible version of some of this.

--- code/cws_toolbox/cws_toolbox/smart_move.pyt.py
+++ code/cws_toolbox/cws_toolbox/smart_move.pyt.py
@@ -1,14 +1,2 @@
 __author__ = 'nrsantos'
 
-from smart_move import *
-
-
-class Toolbox(object):
-	def __init__(self):
-		"""Define the toolbox (the name of the toolbox is the name of the
-		.pyt file)."""
-		self.label = "smart_move"
-		self.alias = "Smart Move Operations"
-
-		# List of tool classes associated with this toolbox
-		self.tools = [SmartMoveFeatures]
